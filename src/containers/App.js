fd10267a462726db0ed02ee613b53e72bf193d6e
Vuonggithubb <fanvuong99@gmail.com>
2024-07-05 20:21:22 +0700

    3

--- src/containers/App.js
+++ src/containers/App.js
@@ -21,6 +21,8 @@ import { CustomToastCloseButton } from '../components/CustomToast';
 
 import HomePage from './HomePage/HomePage.js';
 
+import CustomScrollbars from '../components/CustomScrollbars.js';
+
 class App extends Component {
 
     handlePersistorState = () => {
@@ -49,14 +51,16 @@ class App extends Component {
 
                         {this.props.isLoggedIn && <Header />}
 
-                        <span className="content-container">
-                            <Switch>
-                                <Route path={path.HOME} exact component={(Home)} />
-                                <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
-                                <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
-                                <Route path={path.HOMEPAGE} component={HomePage} />
-                            </Switch>
-                        </span>
+                        <div className="content-container">
+                            <CustomScrollbars style={{ height: '100vh', width: '100%' }}>
+                                <Switch>
+                                    <Route path={path.HOME} exact component={(Home)} />
+                                    <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
+                                    <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
+                                    <Route path={path.HOMEPAGE} component={HomePage} />
+                                </Switch>
+                            </CustomScrollbars>
+                        </div>
 
                         <ToastContainer
                             className="toast-container" toastClassName="toast-item" bodyClassName="toast-item-body"
